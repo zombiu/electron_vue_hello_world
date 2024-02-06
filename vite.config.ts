388509125ade5469532a45ecb705d5b6a4de3571
liurui <303996789@qq.com>
2024-02-06 11:05:39 +0800

    学习搭建electron+vue+ts项目

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import electron from "vite-plugin-electron"
+import electronRenderer from "vite-plugin-electron-renderer"
+import polyfillExports from "vite-plugin-electron-renderer"
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), electron([{
+    // 主进程文件
+    entry: "main/main.ts", 
+  },
+  {
+    entry: 'preload/preload.ts'
+  }
+  ]),
+  electronRenderer(),
+  polyfillExports(),],
+  build: {
+    emptyOutDir: false, // 默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录
+    outDir: "dist-electron"
+  },
+})
